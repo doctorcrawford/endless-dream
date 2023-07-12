a320301400d032332a0d72d64f4163cd5604b4a2
Kyle Crawford <kcraw217@gmail.com>
2023-07-12 15:44:33 -0700

    fix header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,11 @@ const HeaderStyle = styled.div`
   -webkit-background-clip: text;
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
 `
 
 const EndlessDreamHeader = styled.h1`
-  font-size: 50px;
+  font-size: 80px;
   font-weight: 600;
   background-image: url(https://img.freepik.com/free-photo/beautiful-verdigris-oxidized-copper-background_24837-103.jpg?w=2000&t=st=1689197736~exp=1689198336~hmac=ba778ebb02a5a38ebf67f115537041ea7f074f895b1378546bda3b0b0257b9a6);
   background-size: 250px;
@@ -23,15 +24,17 @@ const EndlessDreamHeader = styled.h1`
   -webkit-text-stroke-width: 2px;
   -webkit-text-stroke-color: grey;
   margin-bottom: 0;
+  height: 1em;
 `
 
-const Container = styled.div`
+const Container = styled.p`
   font-size: 20px;
   font-weight: 100;
-  width: fit-content;
   color: grey;
   align-items: center;
   flex-direction: row;
+  backdrop-filter: blur(80px);
+  padding: 1em;
 `
 
 function Header() {
@@ -42,7 +45,7 @@ function Header() {
     <>
       <HeaderStyle>
         <EndlessDreamHeader>
-          <h1>Endless Dream</h1>
+          Endless Dream
         </EndlessDreamHeader>
         <Container>
           <Link to='/' style={padding}>Home</Link>
