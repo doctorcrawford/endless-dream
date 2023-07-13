e71f410468213228e701783a4263edc710879991
Kyle Crawford <kcraw217@gmail.com>
2023-07-13 11:49:54 -0700

    tidy up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,62 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-
-const HeaderStyle = styled.div`
-  background-image: url(https://img.freepik.com/free-photo/beautiful-verdigris-oxidized-copper-background_24837-103.jpg?w=2000&t=st=1689197736~exp=1689198336~hmac=ba778ebb02a5a38ebf67f115537041ea7f074f895b1378546bda3b0b0257b9a6);
-  background-size: 250px;
-  background-repeat: repeat;
-  background-clip: text;
-  -webkit-background-clip: text;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-`
-
-const EndlessDreamHeader = styled.h1`
-  font-size: 85px;
-  font-weight: 600;
-  background-image: url(https://img.freepik.com/free-photo/beautiful-verdigris-oxidized-copper-background_24837-103.jpg?w=2000&t=st=1689197736~exp=1689198336~hmac=ba778ebb02a5a38ebf67f115537041ea7f074f895b1378546bda3b0b0257b9a6);
-  background-size: 250px;
-  background-repeat: repeat;
-  color: transparent;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-stroke: 2px grey;
-  paint-order: stroke fill;
-  margin-bottom: 20px;
-  height: 1em;
-  font-family: Roboto Slab, serif;
-`
-
-const Container = styled.p`
-  font-size: 20px;
-  font-weight: 100;
-  color: grey;
-  align-items: center;
-  flex-direction: row;
-  backdrop-filter: blur(80px);
-  padding: 1em;
-  outline: 2px solid grey;
-  border-radius: 30px;
-  box-shadow: 4px 4px 8px 2px grey;
-  width: 20vw;
-`
-
-const NavbarLink = styled(Link)`
-  color:white;
-  font-size: large;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: none;
-  margin: 10px;
-  &:hover,
-  &:focus{
-    color: rgb(135,206,250);
-  }
-  &:active{
-    color: rgb(148,0,211);
-  }
-`
+import { HeaderStyle, EndlessDreamHeader, NavbarLink } from '../styles/HeaderStyle'
 
 function Header() {
   const padding = {
---
+++ src/styles/HeaderStyle.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const HeaderStyle = styled.div`
+  background-image: url(https://img.freepik.com/free-photo/beautiful-verdigris-oxidized-copper-background_24837-103.jpg?w=2000&t=st=1689197736~exp=1689198336~hmac=ba778ebb02a5a38ebf67f115537041ea7f074f895b1378546bda3b0b0257b9a6);
+  background-size: 250px;
+  background-repeat: repeat;
+  background-clip: text;
+  -webkit-background-clip: text;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`
+
+export const EndlessDreamHeader = styled.h1`
+  font-size: 85px;
+  font-weight: 600;
+  background-image: url(https://img.freepik.com/free-photo/beautiful-verdigris-oxidized-copper-background_24837-103.jpg?w=2000&t=st=1689197736~exp=1689198336~hmac=ba778ebb02a5a38ebf67f115537041ea7f074f895b1378546bda3b0b0257b9a6);
+  background-size: 250px;
+  background-repeat: repeat;
+  color: transparent;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-stroke: 2px grey;
+  paint-order: stroke fill;
+  margin-bottom: 20px;
+  height: 1em;
+  font-family: Roboto Slab, serif;
+`
+
+export const NavbarLink = styled(Link)`
+  color:white;
+  font-size: large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+  &:hover,
+  &:focus{
+    color: rgb(135,206,250);
+  }
+  &:active{
+    color: rgb(148,0,211);
+  }
+`
